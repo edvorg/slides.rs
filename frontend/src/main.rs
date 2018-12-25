b947ed69e50e92957b397c9eb5aaed27135d0d7f
Eduard Knyshov <edvorg@gmail.com>
2018-12-25 15:06:29 +0000

    Simplify hello world

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -6,14 +6,12 @@ fn main() {
     run(
         Story {
             slides: vec!(
-                Slide::Text(String::from("Full-stack web development with Rust")),
-                Slide::Image("rust-logo.png", String::from("Rust is a system programming language")),
+                Slide::Text(String::from("Slides engine in rust")),
+                Slide::Image("rust-logo.png", String::from("Rust logo looks like this")),
                 Slide::List(String::from("Rust strengths"), vec!(
                     String::from("Safety"),
                     String::from("Performance"),
                 )),
-                Slide::Text(String::from("Web assembly is binary executable format")),
-                Slide::Text(String::from("Eduard Knyshov @edvorg")),
                 Slide::Code(String::from("Hello World in Rust"), String::from("fn main() {\n  println!(\"Hello World\");\n}")),
             )
         }
